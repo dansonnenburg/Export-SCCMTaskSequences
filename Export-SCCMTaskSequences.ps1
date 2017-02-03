05829d6be2bc882f7705e65fa7dadba04383cf77
Dan Sonnenburg <Dan.sonnenburg@gmail.com>
2017-02-03 06:03:32 -0600

    Update Export-SCCMTaskSequences.ps1
    Cleaned up some unnecessary lines.
    Removed default variable values.
    Put variables in a parameter block.
    Changed "ComputerName" to "SiteServer".

--- Export-SCCMTaskSequences.ps1
+++ Export-SCCMTaskSequences.ps1
@@ -1,19 +1,15 @@
-﻿$SiteCode = "CM1"
-$ComputerName = "localhost"
-$ExportDir = "\\<ServerName>\ExportedTaskSequences"
-
-# List Task Sequences
-$TsList = Get-WmiObject SMS_TaskSequencePackage -Namespace root\sms\site_$SiteCode -ComputerName $ComputerName| Select *
-$TsList
-
+param (
+    $SiteCode
+    $SiteServer
+    $ExportDir
+)
 # Export Task Sequences
-cd C:
-cd $ExportDir
-#$TsList = Get-WmiObject SMS_TaskSequencePackage
-Get-WmiObject SMS_TaskSequencePackage -Namespace root\sms\site_$SiteCode -ComputerName $ComputerName
+Push-Location $ExportDir
+Get-WmiObject SMS_TaskSequencePackage -Namespace root\sms\site_$SiteCode -ComputerName $SiteServer
 ForEach ($Ts in $TsList)
 {
     $Ts = [wmi]"$($Ts.__PATH)"
     $filename = "$($ts.Name) {0:yyyyMMdd-HHmm}.xml" -f (Get-Date)
     Set-Content -Path $filename -Value $Ts.Sequence
 }
+Pop-Location
